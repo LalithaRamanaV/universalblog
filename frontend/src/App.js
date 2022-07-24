1fd83573c294e85a379b1e6a7b3f8c9c9bbb6bae
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-25 00:32:54 +0530

    feat: created homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import Topbar from "./components/topbar/Topbar";
 import Header from "./components/header/Header"
-import Sidebar from "./components/sidebar/Sidebar";
+// import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
       <Header/>
-      <Sidebar/>
+      {/* <Sidebar/> */}
     </div>
   );
 }
