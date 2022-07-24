ee49d785c326c6add8d6a26043c33ebc39bdd339
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-24 23:16:11 +0530

    feat: created header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
+import Header from "./components/header/Header"
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Header/>
     </div>
   );
 }
