028a3eb912c198b34a6990d626dd18fc8b8bb7b1
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-25 21:20:20 +0530

    feat: created user route

--- api/server.js
+++ api/server.js
@@ -3,19 +3,20 @@ const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose") 
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-   
+    useNewUrlParser: true
 })
 .then(console.log("Connected to MongoDb"))
 .catch((err)=>console.log(err)); 
 
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 
 app.listen("5000",()=>{
