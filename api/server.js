d73c36f085bb5ffa84e7643b927db7f85360fd7d
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-25 20:25:23 +0530

    feat: created and tested login and register routes through postman

--- api/server.js
+++ api/server.js
@@ -2,15 +2,21 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose") 
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
-  
+   
 })
 .then(console.log("Connected to MongoDb"))
-.catch((err)=>console.log(err));
+.catch((err)=>console.log(err)); 
+
+
+app.use("/api/auth", authRoute);
+
 
 app.listen("5000",()=>{
     console.log("Backend server is running")
