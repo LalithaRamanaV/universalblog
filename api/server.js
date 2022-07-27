bca480f1f207f0bcc96234060a186f5f07db41b9
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-07-27 10:39:28 +0530

    feat: connected frontend to backend

--- api/server.js
+++ api/server.js
@@ -39,6 +39,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000",()=>{
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5000,()=>{
     console.log("Backend server is running")
 })
