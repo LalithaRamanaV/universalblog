c3b0b490b8ccb669cae5d624c0104a9bc97f7836
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-01 09:41:18 +0530

    feat: created frontend

--- api/server.js
+++ api/server.js
@@ -39,11 +39,11 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log("Backend server is running")
