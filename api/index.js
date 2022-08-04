a7de95e964e466ecc45f5f789738331aabf5cc39
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-04 18:47:03 +0530

    changes in index

--- api/index.js
+++ api/index.js
@@ -8,14 +8,14 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
-const path = require("node:path");
+const path = require("path");
 
 
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URL,  {
     useNewUrlParser: true
 })
 .then(console.log("Connected to MongoDb"))
@@ -41,7 +41,7 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-// const __dirname = path.resolve();
+
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
